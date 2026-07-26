d4347d885459a418d954402844ba5e11db0dcf07
Aditya Kumar <adk6535@gmail.com>
2026-07-26 18:39:01 +0530

    feat: add healthcare models

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "./config/db.js";
 
-dotenv.config();
-
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on ${PORT}`);
-});
-=======
 // Import dotenv package to load environment variables from the .env file
 import dotenv from "dotenv";
 
@@ -45,4 +31,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
->>>>>>> 18731c1e572976ee0c0efff05250075da39d744f
